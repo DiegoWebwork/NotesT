2a580750d9d10997daa06f5bed12c08728b0a247
Diego <e_guinho@hotmail.com>
2024-05-26 21:53:26 -0300

    update

---
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return <>
+        <Navbar />
+
+        
+        <div>
+            <div>
+                <form onSubmit={() => {}}>
+                    <h4 className="text-2xl mb-7">Login</h4>
+
+                    <input type="text" placeholder="Email" className="input-box" />
+                    
+                    <button type="submit" className="btn-primary">
+                    Login
+                    </button>
+                    
+                    <p className="text-sm text-center mt-4">
+                      Not registered yet?{" "}
+                        <Link to="/signup" className="">
+                            Create an Account
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </>;
+    
+};
+
+export default Login;
