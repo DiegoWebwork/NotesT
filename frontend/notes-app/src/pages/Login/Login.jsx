46aa9bb7240a3823104d875bcbd1fb3e48cbd0e5
Diego <e_guinho@hotmail.com>
2024-05-26 22:10:25 -0300

    Update Login

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -7,8 +7,8 @@ const Login = () => {
         <Navbar />
 
         
-        <div>
-            <div>
+        <div className="flex items-center justify-center mt-28">
+            <div className="w-96 border rounded bg-white px-7 py-10">
                 <form onSubmit={() => {}}>
                     <h4 className="text-2xl mb-7">Login</h4>
 
@@ -20,7 +20,7 @@ const Login = () => {
                     
                     <p className="text-sm text-center mt-4">
                       Not registered yet?{" "}
-                        <Link to="/signup" className="">
+                        <Link to="/signup" className="font-medium text-primary underline">
                             Create an Account
                         </Link>
                     </p>
